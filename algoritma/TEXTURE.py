45fd78e7ab63e889e04d42a5b9d516fb7479f0a4
WazeAzure <bryan.rich0604@gmail.com>
2023-11-15 18:03:42 +0700

    Add: Finalized algorithm in python

--- algoritma/TEXTURE.py
+++ algoritma/TEXTURE.py
@@ -1,10 +1,12 @@
 import time
 from cv2 import imread, imwrite
 import numpy as np
+from numpy.linalg import norm
 from colorama import Fore, Back, Style
 import sys
 from math import sqrt, pow, log
 np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(linewidth=np.inf)
 
 def write_to_img(fname, data):
     imwrite(fname, data)
@@ -20,46 +22,50 @@ def toBlack(img):
 
     B, G, R = img[:, :, 0], img[..., 1], img[..., 2]
     black = 0.299 * R + 0.587 * G + 0.114 * B
-    black = np.where(black <= 255, black, 255)
     return black
 
 def feature_extraction(fm):
-    contrast = 0
-    for i in range(fm.shape[0]):
-        for j in range(fm.shape[1]):
-            contrast += (fm[i][j] * pow(i - j, 2))
+    matrix_index = np.arange(fm.shape[0])
+    m_square = np.square(matrix_index[:, None] - matrix_index)
 
-    homogenity = 0
-    for i in range(fm.shape[0]):
-        for j in range(fm.shape[1]):
-            homogenity += (fm[i][j] / (1 + pow(i-j, 2)))
+    contrast = np.sum(m_square * fm)
+    # for i in range(fm.shape[0]):
+    #     for j in range(fm.shape[1]):
+    #         contrast += (fm[i][j] * pow(i - j, 2))
 
-    entropy = 0
-    for i in range(fm.shape[0]):
-        for j in range(fm.shape[1]):
-            if(fm[i][j] == 0):
-                entropy += 0
-                continue
-            entropy += fm[i][j] * log(fm[i][j])
-    entropy = -entropy
+    homogenity = np.sum(fm / (1 + m_square))
+    # for i in range(fm.shape[0]):
+    #     for j in range(fm.shape[1]):
+    #         homogenity += (fm[i][j] / (1 + pow(i-j, 2)))
 
-    dissimilarity = 0
-    for i in range(fm.shape[0]):
-        for j in range(fm.shape[1]):
-            dissimilarity += fm[i][j] * abs(i-j)
+    entropy = -np.sum(np.where(fm == 0, 0, fm * np.log(fm)))
+    # for i in range(fm.shape[0]):
+    #     for j in range(fm.shape[1]):
+    #         if(fm[i][j] == 0):
+    #             entropy += 0
+    #             continue
+    #         entropy += (fm[i][j] * log(fm[i][j]))
+    # entropy = -entropy
 
-    asm = 0
-    for i in range(fm.shape[0]):
-        for j in range(fm.shape[1]):
-            asm += pow(fm[i][j], 2)
-    energy = sqrt(asm)
-    return np.array([contrast, homogenity, entropy, dissimilarity, asm, energy])
+    # dissimilarity = 0
+    # for i in range(fm.shape[0]):
+    #     for j in range(fm.shape[1]):
+    #         dissimilarity += fm[i][j] * abs(i-j)
+
+    # asm = 0
+    # for i in range(fm.shape[0]):
+    #     for j in range(fm.shape[1]):
+    #         asm += pow(fm[i][j], 2)
+    # energy = sqrt(asm)
+
+    # dissimilarity, asm, energy
+    return np.array([contrast, homogenity, entropy])
 
 def createCoOccurence(arr):
     framework0 = np.zeros((256, 256))
-    framework45 = np.zeros((256, 256))
-    framework90 = np.zeros((256, 256))
-    framework135 = np.zeros((256, 256))
+    # framework45 = np.zeros((256, 256))
+    # framework90 = np.zeros((256, 256))
+    # framework135 = np.zeros((256, 256))
     
     # 0 angle
     for i in range(arr.shape[0]):
@@ -71,58 +77,51 @@ def createCoOccurence(arr):
     # write_to_file("texture-0angle.txt", framework0)
 
     # 45 angle
-    for i in range(1, arr.shape[0]):
-        for j in range(0, arr.shape[1]-1):
-            x = arr[i][j]
-            y = arr[i-1][j+1]
-            # print(x, y)
-            framework45[x][y] += 1
+    # for i in range(1, arr.shape[0]):
+    #     for j in range(0, arr.shape[1]-1):
+    #         x = arr[i][j]
+    #         y = arr[i-1][j+1]
+    #         # print(x, y)
+    #         framework45[x][y] += 1
 
     # 90 angle
-    for i in range(1, arr.shape[0]):
-        for j in range(arr.shape[1]):
-            x = arr[i][j]
-            y = arr[i-1][j]
-            framework90[x][y] += 1
+    # for i in range(1, arr.shape[0]):
+    #     for j in range(arr.shape[1]):
+    #         x = arr[i][j]
+    #         y = arr[i-1][j]
+    #         framework90[x][y] += 1
 
     # 135 angle
-    for i in range(1, arr.shape[0]):
-        for j in range(1, arr.shape[1]):
-            x = arr[i][j]
-            y = arr[i-1][j-1]
-            framework135[x][y] += 1
+    # for i in range(1, arr.shape[0]):
+    #     for j in range(1, arr.shape[1]):
+    #         x = arr[i][j]
+    #         y = arr[i-1][j-1]
+    #         framework135[x][y] += 1
     framework0 += np.transpose(framework0)
-    framework45 += np.transpose(framework45)
-    framework90 += np.transpose(framework90)
-    framework135 += np.transpose(framework135)
+    # framework45 += np.transpose(framework45)
+    # framework90 += np.transpose(framework90)
+    # framework135 += np.transpose(framework135)
 
     
     # normalize
-    framework0 = framework0 / np.sum(np.sum(framework0))
-    framework45 = framework45 / np.sum(np.sum(framework45))
-    framework90 = framework90 / np.sum(np.sum(framework90))
-    framework135 = framework135 / np.sum(np.sum(framework135))
+    framework0 = framework0 / (np.sum(framework0))
+    # framework45 = framework45 / (np.sum(framework45))
+    # framework90 = framework90 / (np.sum(framework90))
+    # framework135 = framework135 / (np.sum(framework135))
     # print(framework0)
 
+    
     f_0 = feature_extraction(framework0)
-    f_45 = feature_extraction(framework45)
-    f_90 = feature_extraction(framework90)
-    f_135 = feature_extraction(framework135)
+    # f_45 = feature_extraction(framework45)
+    # f_90 = feature_extraction(framework90)
+    # f_135 = feature_extraction(framework135)
 
-    return f_0, f_45, f_90, f_135
+    return f_0 #, f_45, f_90, f_135
 
 def cosine_similarity(v1, v2):
-    # print(v1, v2)
-    mat_jumlah_dot = np.sum(v1 * v2)
-    # print(mat_jumlah_dot)
-    # exit()
+    hasil_dot = np.dot(v1, v2)
 
-    # print(mat_jumlah_dot[0][0])
-
-    kali_dot1 = np.sqrt(np.sum(v1 * v1))
-    kali_dot2 = np.sqrt(np.sum(v2 * v2))
-
-    return ((mat_jumlah_dot) / (kali_dot1 * kali_dot2))
+    return ((hasil_dot) / (norm(v1) * norm(v2)))
 
 def processPicture(f1, f2):
     img = imread(f1)
@@ -134,7 +133,8 @@ def processPicture(f1, f2):
 
     # write_to_file("angie_apple_black.txt", img)
     
-    fm0, fm45, fm90, fm135 = createCoOccurence(img)
+    # fm0, fm45, fm90, fm135 = createCoOccurence(img)
+    fm0 = createCoOccurence(img)
 
     img2 = imread(f2)
     img2 = toBlack(img2)
@@ -147,15 +147,34 @@ def processPicture(f1, f2):
     # write_to_file("angie_apple_black.txt", img2)
     
     
-    fm20, fm245, fm290, fm2135 = createCoOccurence(img2)
+    # fm20, fm245, fm290, fm2135 = createCoOccurence(img2)
+    fm20 = createCoOccurence(img2)
 
+    # print(fm20)
+    # print(type(fm20))
+    print()
+    print(f1)
+    print("------------------------------")
+    print("contrast, homogenity, entropy, dissimilarity, asm, energy")
+    print(fm0)
+    # print(fm45)
+    # print(fm90)
+    # print(fm135)
+
+    print()
+    print(f2)
+    print("------------------------------")
+    print("contrast, homogenity, entropy, dissimilarity, asm, energy")
     print(fm20)
-    print(type(fm20))
+    # print(fm245)
+    # print(fm290)
+    # print(fm2135)
 
+    print()
     print("Simmilarity 0 degree:", cosine_similarity(fm0, fm20))
-    print("Simmilarity 45 degree:", cosine_similarity(fm45, fm245))
-    print("Simmilarity 90 degree:", cosine_similarity(fm90, fm290))
-    print("Simmilarity 135 degree:", cosine_similarity(fm135, fm2135))
+    # print("Simmilarity 45 degree:", cosine_similarity(fm45, fm245))
+    # print("Simmilarity 90 degree:", cosine_similarity(fm90, fm290))
+    # print("Simmilarity 135 degree:", cosine_similarity(fm135, fm2135))
 
 
 
@@ -167,8 +186,8 @@ def main(f1, f2):
 if __name__ == "__main__" :
     # f1 = "images/angie_apel.jpg"
     # f2 = "images/angie_nct.jpg"
-    f1 = "images/lena.png"
-    f2 = "images/angie_apel.jpg"
+    f1 = "images/Solid_black.png"
+    f2 = "images/putih2_0.png"
 
     print(f1, f2)
     print()
